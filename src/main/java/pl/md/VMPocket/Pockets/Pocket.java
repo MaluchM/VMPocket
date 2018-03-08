8995777e63db565f8b6ea1d456d6f4fe84f47dac
iMac Magda <magda@ssolutions.com.pl>
2018-03-08 13:41:23 +0100

    Initial commit

---
+++ src/main/java/pl/md/VMPocket/Pockets/Pocket.java
@@ -0,0 +1,4 @@
+package pl.md.VMPocket.Pockets;
+
+public class Pocket {
+}
---
+++ src/test/java/pl/md/VMPocket/Pockets/PocketTest.java
@@ -0,0 +1,13 @@
+package pl.md.VMPocket.Pockets;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PocketTest {
+
+    @Test
+    public void shouldCreateNewPocket() {
+
+    }
+}
